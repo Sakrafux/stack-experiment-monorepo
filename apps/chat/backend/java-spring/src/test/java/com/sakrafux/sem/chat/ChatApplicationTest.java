532a1c000e58000d408abd0098186790be1119d5
Andreas Hell <andhell03@gmail.com>
2023-07-04 22:01:38 +0200

    Inital commit with multiple Java projects

---
+++ apps/chat/backend/java-spring/src/test/java/com/sakrafux/sem/chat/ChatApplicationTest.java
@@ -0,0 +1,13 @@
+package com.sakrafux.sem.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ apps/realworld/backend/java-spring/src/test/java/com/sakrafux/sem/realworld/RealworldApplicationTest.java
@@ -0,0 +1,13 @@
+package com.sakrafux.sem.realworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealworldApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
