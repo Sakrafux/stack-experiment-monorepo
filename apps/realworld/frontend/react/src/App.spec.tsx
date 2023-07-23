0dcb18b50c7a5a35daffd299f008da1ea2ce63d5
Andreas Hell <andhell03@gmail.com>
2023-07-23 15:00:30 +0200

    added some exemplary testing

--- apps/realworld/frontend/react/src/App.spec.tsx
+++ apps/realworld/frontend/react/src/App.spec.tsx
@@ -1,16 +1,14 @@
 import { render } from '@testing-library/react';
-
-import { BrowserRouter } from 'react-router-dom';
-
 import App from './App';
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  BrowserRouter: () => <div>Router</div>,
+}));
 
 describe('App', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    );
+    const { baseElement } = render(<App />);
     expect(baseElement).toBeTruthy();
   });
 });
